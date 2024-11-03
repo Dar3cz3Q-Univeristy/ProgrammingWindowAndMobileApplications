04795153408f5dfc464d893ae74973a2b544f2a3
Dariusz Homa <dariuszhoma7@gmail.com>
2024-11-03 14:56:18 +0100

    Lab 2 almost done

---
+++ Lab 2/src/main/java/org/example/ClassTeacher.java
@@ -0,0 +1,134 @@
+package org.example;
+
+import java.util.*;
+
+public class ClassTeacher {
+    private final String teacherGroupName;
+    private final Set<Teacher> teachersList = new HashSet<>();
+    private final int maxTeachersCapacity;
+
+    public ClassTeacher(String teacherGroupName, int maxTeachersCapacity) {
+        this.teacherGroupName = teacherGroupName;
+        this.maxTeachersCapacity = maxTeachersCapacity;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        int listSize = teachersList.size();
+
+        if (listSize >= maxTeachersCapacity) {
+            System.out.println("List nauczycieli jest pelna");
+            return;
+        }
+
+        if (getTeacherFromList(teacher) != null) {
+            System.out.println("Nauczyciel juz istnieje");
+            return;
+        }
+
+        teachersList.add(teacher);
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        Teacher tempTeacher = getTeacherFromList(teacher);
+
+        if (tempTeacher == null) {
+            System.out.println("Nauczyciel nie istnieje w liscie");
+            return;
+        }
+
+        teachersList.remove(tempTeacher);
+    }
+
+    public void addSalary(Teacher teacher, double salary) {
+        Teacher tempTeacher = getTeacherFromList(teacher);
+
+        if (tempTeacher == null) {
+            System.out.println("Nauczyciel nie istnieje w liscie");
+            return;
+        }
+
+        tempTeacher.updateSalary(salary);
+    }
+
+    public void changeCondition(Teacher teacher, TeacherConditionEnum condition) {
+        Teacher tempTeacher = getTeacherFromList(teacher);
+
+        if (tempTeacher == null) {
+            System.out.println("Nauczyciel nie istnieje w liscie");
+            return;
+        }
+
+        tempTeacher.updateCondition(condition);
+    }
+
+    public Teacher search(String surname) {
+
+        for (Teacher teacher1 : teachersList) {
+            if (teacher1.compare(surname) == 0)
+                return teacher1;
+        }
+
+        return null;
+    }
+
+    public List<Teacher> searchPartial(String str) {
+        List<Teacher> result = new ArrayList<>();
+
+        for (Teacher teacher1 : teachersList) {
+            if (teacher1.contains(str))
+                result.add(teacher1);
+        }
+
+        return result;
+    }
+
+    public int countByCondition(TeacherConditionEnum condition) {
+        int result = 0;
+
+        for (Teacher teacher1 : teachersList) {
+            if (teacher1.compare(condition) == 0)
+                result++;
+        }
+
+        return result;
+    }
+
+    public List<Teacher> sortByName() {
+        var teachersList = new ArrayList<>(this.teachersList);
+        teachersList.sort(Teacher.Comparators.NAME);
+        return teachersList;
+    }
+
+    public List<Teacher> sortBySalary() {
+        var teachersList = new ArrayList<>(this.teachersList);
+        teachersList.sort(Teacher.Comparators.SALARY);
+        return teachersList;
+    }
+
+    public Teacher max() {
+        var teachersList = new ArrayList<>(this.teachersList);
+        return Collections.max(teachersList, Teacher.Comparators.SALARY);
+    }
+
+    public int count() {
+        return teachersList.size();
+    }
+
+    public int getMaxTeachersCapacity() {
+        return maxTeachersCapacity;
+    }
+
+    private Teacher getTeacherFromList(Teacher teacher) {
+        for (Teacher teacher1 : teachersList) {
+            if (teacher1.equals(teacher))
+                return teacher1;
+        }
+
+        return null;
+    }
+
+    public void summary() {
+        System.out.println(teacherGroupName);
+        for (Teacher teacher : teachersList) teacher.printing();
+    }
+}
---
+++ Lab 2/src/main/java/org/example/Teacher.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class Teacher implements Comparable<Teacher> {
+    private final String name;
+    private final String surname;
+    private TeacherConditionEnum condition;
+    private final int yearOfBirth;
+    private double salary;
+
+    public Teacher(String name, String surname, TeacherConditionEnum condition, int yearOfBirth, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.condition = condition;
+        this.yearOfBirth = yearOfBirth;
+        this.salary = salary;
+    }
+
+    public void printing() {
+        System.out.println("Imie nauczyciela: " + name);
+        System.out.println("Nazwisko nauczyciela: " + surname);
+        System.out.println("Stan nauczyciela: " + condition.toString());
+        System.out.println("Rok urodzenia: " + yearOfBirth);
+        System.out.println("Wynagrodzenie: " + salary);
+    }
+
+    public void updateSalary(double salary) {
+        this.salary += salary;
+    }
+
+    public void updateCondition(TeacherConditionEnum condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public int compareTo(Teacher o) {
+        return Comparators.SURNAME.compare(this, o);
+    }
+
+    public boolean equals(Teacher o) {
+        if (!this.surname.equals(o.surname)) return false;
+        if (!this.name.equals(o.name)) return false;
+        return this.yearOfBirth == o.yearOfBirth;
+    }
+
+    public int compare(String surname) {
+        return this.surname.compareTo(surname);
+    }
+
+    public int compare(TeacherConditionEnum condition) {
+        return this.condition.compareTo(condition);
+    }
+
+    public boolean contains(String str) {
+        return this.surname.contains(str) || this.name.contains(str);
+    }
+
+    public static class Comparators {
+
+        public static Comparator<Teacher> NAME = Comparator.comparing(o -> o.name);
+
+        public static Comparator<Teacher> SURNAME = Comparator.comparing(o -> o.surname);
+
+        public static Comparator<Teacher> SALARY = (o1, o2) -> (int) (o1.salary - o2.salary);
+    }
+}
