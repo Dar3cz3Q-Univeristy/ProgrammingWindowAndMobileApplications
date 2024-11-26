0cb9f59c7e3b3960ba22bb34b64c4c3891904ee0
Dariusz Homa <dariuszhoma7@gmail.com>
2024-11-26 21:47:32 +0100

    Added files and docker

---
+++ Lab 4/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+// Łączenie z bazą przez JDBC
+// Prosty CRUD
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
